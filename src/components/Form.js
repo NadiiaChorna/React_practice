eae5a0d290854179915cda8f9322cf9e89fa92b4
Nadiia Chorna <nadijagopko@gmail.com>
2019-09-23 23:49:46 +0300

    25 minute

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Form = () => (
+    <form>
+        <div className="form-goup">
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your note here"
+            />
+        </div>
+    </form>
+);
